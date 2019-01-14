5991dd6fd7f99f108a3374185cc8413bc83d66b0
rpanday <rpanebay2@gmail.com>
2019-01-14 19:36:22 +0000

    model.h5 trained for 105 mins, ~45k samples, fails at waterbody turn'

--- model.py
+++ model.py
@@ -1,63 +1,106 @@
 import csv
 import cv2
-import os.path
+import os.path 
 import numpy as np
+import sklearn
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Cropping2D, Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from sklearn.model_selection import train_test_split
 
-def load_data(data_folder):
+#don't use load_data as it loads all images in memory at once, use generators instead
+def load_data():
     lines = []
     with open('{}/driving_log.csv'.format(data_folder)) as csvfile:
         for line in csv.reader(csvfile):
             lines.append(line)
-    
+    return process_log_lines(lines)
+
+def process_log_lines(lines):   
     images = []
     measurements = []
     for line in lines:
-        path = line[0]
-        filename = path.split('/')[-1]
-        current_path = '{}/IMG/{}'.format(data_folder,filename)
-        if not os.path.isfile(current_path):
-            continue
-        image = cv2.cvtColor(cv2.imread(current_path), cv2.COLOR_BGR2RGB)
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
+        skip = False
+        for i in range(3): #center, left, right
+            path = line[i]
+            filename = path.split('/')[-1]
+            current_path = '{}/IMG/{}'.format(data_folder, filename)
+            if os.path.isfile(current_path):
+                image = cv2.cvtColor(cv2.imread(current_path), cv2.COLOR_BGR2RGB) #convert for drive.py
+                images.append(image)
+            else:
+                skip = True
+                i = 2
+        if not skip:
+            correction = 0.2
+            measurement = float(line[3])
+            measurements.append(measurement)
+            measurements.append(measurement + correction)
+            measurements.append(measurement - correction)
     
     X_train = np.array(images)
     y_train = np.array(measurements)
     return X_train, y_train
 
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        #shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            X_train, y_train = process_log_lines(batch_samples)
+            yield sklearn.utils.shuffle(X_train, y_train)
 
-def test_model():
-    model = Sequential()
-    model.add(Flatten(input_shape=(160,320,3)))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
-    return model
-    
+def get_input_paths():
+    samples = []
+    with open('{}/driving_log.csv'.format(data_folder)) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            samples.append(line)
+    return samples
+
+
 def lenet5_model(input_shape):
     model = Sequential()
     model.add(Lambda (lambda x: x/255.0 - 0.5, input_shape=input_shape))
     model.add(Cropping2D(cropping=((70,25), (0,0))))
-    model.add(Convolution2D(6, 5, 5, activation="relu"))
+    model.add(Convolution2D(6,5,5,activation='relu'))
     model.add(MaxPooling2D())
-    model.add(Convolution2D(16, 5, 5, activation="relu"))
+    model.add(Convolution2D(16,5,5,activation='relu'))
     model.add(MaxPooling2D())
+
     model.add(Flatten())
+
     model.add(Dense(120, activation='relu'))
     model.add(Dense(84, activation='relu'))
     model.add(Dense(1))
+
     model.compile(loss='mse', optimizer='adam')
+
     return model
 
-# data_path = './data'
-data_path = '../train'
-X_train, y_train = load_data(data_path)
-# model = test_model()
-model = lenet5_model((160,320,3))
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10) 
-model.save('model.h5')
 
+def main():
+    samples = get_input_paths()
+    train_samples, validation_samples = train_test_split(samples, test_size=test_size)
+    train_generator = generator(train_samples, batch_size=batch_size)
+    validation_generator = generator(validation_samples, batch_size=batch_size)
+
+    ch, row, col = 3, 160, 320 
+    model = lenet5_model((row, col, ch))
+    model.fit_generator(train_generator, samples_per_epoch= len(train_samples), 
+                        validation_data=validation_generator, 
+                        nb_val_samples=len(validation_samples), nb_epoch=epochs)
+
+    # X_train, y_train = load_data()
+    # model = lenet5_model((row, col, ch))
+    # model.fit(X_train, y_train, validation_split=validation_split, shuffle=True, epochs=epoch)
+    model.save('model.h5')
+
+data_folder = './data'
+batch_size = 32
+test_size = 0.2
+# validation_split = 0.2
+epochs = 7
+main()
